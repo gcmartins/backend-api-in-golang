d99217f43c22185fded15a3d44b3d92948fff68e
Gustavo Martins <gucmartins@gmail.com>
2023-07-27 23:14:06 -0300

    Setup CORS policy

--- routes/routes.go
+++ routes/routes.go
@@ -3,12 +3,14 @@ package routes
 import (
 	"MileTravel/controllers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func LoadRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/", controllers.Index)
+	r.Use(cors.Default())
+	r.GET("/api/testimonials-home", controllers.TestimonialsHome)
 	r.GET("/api/testimonials", controllers.Testimonials)
 	r.POST("/api/testimonials", controllers.CreateTestimonial)
 	r.GET("/api/testimonials/:id", controllers.TestimonialById)
